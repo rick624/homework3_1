0832bb228755fac62d1b9d0b4dda702cfea567f6
rick624 <rick624@so-net.net.tw>
2023-11-24 12:59:55 +0800

    function design, toast, dialog

--- app/src/main/java/com/rick/homework3_1/MainActivity.kt
+++ app/src/main/java/com/rick/homework3_1/MainActivity.kt
@@ -2,10 +2,46 @@ package com.rick.homework3_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
+    val secretNumber = SecretNumber()
+
+    lateinit var edNumber : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "secret: " + secretNumber.secret)
+        findViews()
+    }
+
+    private fun findViews() {
+        edNumber = findViewById(R.id.ed_number)
+    }
+
+    fun check(view: View){
+        val n = edNumber.text.toString().toInt()
+        val diff = secretNumber.validate(n)
+        var message = "Excellent! The number is"
+
+//        println("number: $n")
+        Log.d("MainActivity", "number:" +n)
+        if (diff < 0){
+            message = "Bigger"
+        }else if (diff > 0){
+            message = "Smaller"
+        }else{
+            message = "$message ${secretNumber.secret}"
+        }
+//        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+            .setTitle("Message")
+            .setMessage(message)
+            .setPositiveButton("ok", null)
+            .show()
     }
 }
